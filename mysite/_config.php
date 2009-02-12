b148ddf16eb4a52551850f3efd170afef0459618
phalkunz <phalkunz@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-02-12 02:54:37 +0000

    MINOR: refactored - move mollom method to MollomServer so that later we can add server error/exception handling without touching MollomField
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/phpinstaller/branches/2.3@71749 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,12 +4,16 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = "";
+$database = "SS_ssinstaller2_3";
 
 require_once("conf/ConfigureFromEnv.php");
 
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.com/themes/
 SSViewer::set_theme('blackcandy');
+
+Mollom::setPublicKey("1819023dfcb10a667d10bd1578c5f39b");
+Mollom::setPrivateKey("2430fcbd65756ac6732a54a7c9ada116");
+SpamProtecterManager::set_spam_protecter('MollomSpamProtector')
 
 ?>
